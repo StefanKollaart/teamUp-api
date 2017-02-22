b8f2bf47eb118d9b5a622c58f44cf473da541319
StefanKollaart <garbera@icloud.com>
2017-02-22 21:30:33 +0100

    ClassDay model

--- src/services/classDay/hooks/index.js
+++ src/services/classDay/hooks/index.js
@@ -3,23 +3,43 @@
 const globalHooks = require('../../../hooks');
 const hooks = require('feathers-hooks');
 const auth = require('feathers-authentication').hooks;
+const common = require('feathers-hooks-common');
+
+
+const populateallStudents = common.populate('allStudents', {service: 'users'})
+const populatepickableStudents = common.populate('pickableStudents', {service: 'users'})
 
 exports.before = {
-  all: [
+  all: [],
+  find: [],
+  get: [],
+  create: [
     auth.verifyToken(),
     auth.populateUser(),
     auth.restrictToAuthenticated()
   ],
-  find: [],
-  get: [],
-  create: [],
-  update: [],
-  patch: [],
-  remove: []
+  update: [
+    auth.verifyToken(),
+    auth.populateUser(),
+    auth.restrictToAuthenticated()
+  ],
+  patch: [
+    auth.verifyToken(),
+    auth.populateUser(),
+    auth.restrictToAuthenticated()
+  ],
+  remove: [
+    auth.verifyToken(),
+    auth.populateUser(),
+    auth.restrictToAuthenticated()
+  ]
 };
 
 exports.after = {
-  all: [],
+  all: [
+    populatepickableStudents,
+    populateallStudents,
+  ],
   find: [],
   get: [],
   create: [],
